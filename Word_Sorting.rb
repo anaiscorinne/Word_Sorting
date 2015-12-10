a31383fc4c5c5d837e703d4551d4955ede47adfe
Anais Corinne Engelmajer <anaiscorinne@gmail.com>
2015-12-09 22:40:43 -0500

    Able to create a function turning a string into an array and sorting the elements of the array

--- Word_Sorting.rb
+++ Word_Sorting.rb
@@ -1,4 +1,5 @@
-array = []
-str = "Have a nice day."
-puts (array << str.split(/ /))
-
+def array_to_sentence(string)
+	string = string.split
+	puts string.sort
+end
+array_to_sentence("Have a nice day.")
