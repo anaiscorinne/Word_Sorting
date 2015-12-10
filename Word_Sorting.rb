5b762a00484552f5d35a95222950be244ff3e64c
Anais Corinne Engelmajer <anaiscorinne@gmail.com>
2015-12-10 11:47:30 -0500

    Used curly braces instead of do -> end

--- Word_Sorting.rb
+++ Word_Sorting.rb
@@ -1,6 +1,6 @@
 def array_to_sentence(string)
 	string = string.split(/\W/)
-	return string.sort do |a,b| a.upcase <=> b.upcase end
+	return string.sort { |a,b| a.upcase <=> b.upcase }
 end
 puts array_to_sentence("Have a nice day.")
 
