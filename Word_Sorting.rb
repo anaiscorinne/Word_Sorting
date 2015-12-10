67a5094e078416718f88b6b4f4839102fc80a6d3
Anais Corinne Engelmajer <anaiscorinne@gmail.com>
2015-12-10 11:49:42 -0500

    Second try with a different sentence

--- Word_Sorting.rb
+++ Word_Sorting.rb
@@ -2,5 +2,5 @@ def array_to_sentence(string)
 	string = string.split(/\W/)
 	return string.sort { |a,b| a.upcase <=> b.upcase }
 end
-puts array_to_sentence("Have a nice day.")
+puts array_to_sentence("What the hell are you talking about, I finally figured this out!")
 
