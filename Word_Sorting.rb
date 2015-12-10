f764558ece0b52f22bbac3ab8a54266b24522f42
Anais Corinne Engelmajer <anaiscorinne@gmail.com>
2015-12-10 11:45:32 -0500

    Used a do rule after sort that allowed the function to ignore cases

--- Word_Sorting.rb
+++ Word_Sorting.rb
@@ -1,5 +1,6 @@
 def array_to_sentence(string)
 	string = string.split(/\W/)
-	return string.sort
+	return string.sort do |a,b| a.upcase <=> b.upcase end
 end
 puts array_to_sentence("Have a nice day.")
+
